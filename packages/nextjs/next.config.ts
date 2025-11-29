6f10a43fe726b02e9514c6d86a82a5782a49fdcf
五十嵐翔 <igara1119@gmail.com>
2025-11-29 14:28:53 +0900

    GitHub Pagesの設定追加

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/multi-fw-demo/nextjs",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
